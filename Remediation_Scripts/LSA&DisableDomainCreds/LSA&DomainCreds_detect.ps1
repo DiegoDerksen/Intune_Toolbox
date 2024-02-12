e52a773967814e5f737265895ed54535254ef39b
Diégo Derksen <144729242+DiegoDerksen@users.noreply.github.com>
2024-02-12 17:05:01 +0100

    Create LSA&DomainCreds_detect.ps1

---
+++ Remediation_Scripts/LSA&DisableDomainCreds/LSA&DomainCreds_detect.ps1
@@ -0,0 +1,23 @@
+<#
+    .SYNOPSIS
+        Detect whether LSA Protection & DisableDomainCreds are set in registry
+
+    .NOTES
+        Author: Diégo Derksen
+        linkedIn: www.linkedin.com/in/diego-derksen
+#>
+$RegistryPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa"
+$RegistryValue1 = "RunAsPPL"
+$ExpectedValue1 = 1
+$RegistryValue2 = "DisableDomainCreds"
+$ExpectedValue2 = 1
+
+$Properties = Get-ItemProperty -Path $RegistryPath
+
+if ($Properties.$RegistryValue1 -eq $ExpectedValue1 -and $Properties.$RegistryValue2 -eq $ExpectedValue2) {
+    Write-Host "Beide registry keys bestaan met de verwachte waarden."
+    exit 0  # Success
+} else {
+    Write-Host "Een of beide registry keys bestaan niet of hebben niet de verwachte waarden."
+    exit 1  # Failure
+}
