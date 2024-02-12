c97775d985462b7876e06088fe70487d0c40b89a
Diégo Derksen <144729242+DiegoDerksen@users.noreply.github.com>
2024-02-12 17:05:29 +0100

    Create LSA&DomainCreds_remediate.ps1

---
+++ Remediation_Scripts/LSA&DisableDomainCreds/LSA&DomainCreds_remediate.ps1
@@ -0,0 +1,20 @@
+<#
+    .SYNOPSIS
+        Remediate LSA Protection and DisableDomainCreds in registry.
+
+    .NOTES
+        Author: Diégo Derksen
+        linkedIn: www.linkedin.com/in/diego-derksen
+#>
+
+$RegistryPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa"
+$RegistryValue1 = "RunAsPPL"
+$ExpectedValue1 = 1
+$RegistryValue2 = "DisableDomainCreds"
+$ExpectedValue2 = 1
+
+Set-ItemProperty -Path $RegistryPath -Name $RegistryValue1 -Value $ExpectedValue1
+Set-ItemProperty -Path $RegistryPath -Name $RegistryValue2 -Value $ExpectedValue2
+    
+Write-Host "Registry keys zijn veranderd naar de juiste waarde."
+Exit 0
