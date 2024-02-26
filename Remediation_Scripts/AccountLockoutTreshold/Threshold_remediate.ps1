0ceec7d5b774688e32e5eb4f472d9e574b4bd2d2
Diégo Derksen <144729242+DiegoDerksen@users.noreply.github.com>
2024-02-26 15:11:20 +0100

    Update Threshold_remediate.ps1

--- Remediation_Scripts/AccountLockoutTreshold/Threshold_remediate.ps1
+++ Remediation_Scripts/AccountLockoutTreshold/Threshold_remediate.ps1
@@ -1,13 +1,15 @@
 <#
     .SYNOPSIS
-       If not detected at 10 attempts, remediate to 10.
+       If not detected at 10 or 15 attempts, remediate to 10 and 15.
 
     .NOTES
         Author: Diégo Derksen
         linkedIn: www.linkedin.com/in/diego-derksen
 #>
     # Set the lockout threshold to 10
-    net accounts /lockoutthreshold:10
+net accounts /lockoutthreshold:10
+net accounts /lockoutduration:15
+net accounts /lockoutwindow:15
 
-    Write-Host "Threshold gewijzigd naar 10"
-    exit 0
+Write-Host "Waardes gewijzigd"
+Exit 0
